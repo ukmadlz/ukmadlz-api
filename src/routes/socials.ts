2b5806e7de4352ae3d8363d4b064f05603b78b56
Mike Elsmore <mike@elsmore.me>
2024-07-05 15:22:49 -0600

    Add a break to test

--- src/routes/socials.ts
+++ src/routes/socials.ts
@@ -40,7 +40,7 @@ export default async function (fastify: FastifyInstance) {
                 },
               },
               generated: {
-                type: "string",
+                type: "number",
               },
             },
           },
